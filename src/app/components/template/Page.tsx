b2a810753b9ecbf8dc719416e731b1ee97526c93
Sr. Kobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-03-27 18:57:27 -0300

    separate folders

---
+++ src/app/components/template/Page.tsx
@@ -0,0 +1,11 @@
+export interface PageProps {
+  children: any;
+}
+
+export default function Page(props: PageProps) {
+  return (
+    <>
+      <div>{props.children}</div>
+    </>
+  );
+}
---
+++ src/app/data/constants/users.ts
@@ -0,0 +1,40 @@
+import { User } from "@/core/model/User";
+
+const users: User[] = [
+  {
+    id: "1",
+    email: "ana@empresa.com",
+    name: "Ana",
+    password: "123454",
+  },
+  {
+    id: "2",
+    email: "pedro@empresa.com",
+    name: "Pedro",
+    password: "1234542",
+  },
+  {
+    id: "3",
+    email: "jorge@empresa.com",
+    name: "Jorge",
+    password: "12345433",
+  },
+  {
+    id: "4",
+    email: "julio@empresa.com",
+    name: "Julio",
+    password: "1234545678",
+  },
+  {
+    id: "5",
+    email: "gustavo@empresa.com",
+    name: "Gustavo",
+    password: "1234545678910",
+  },
+  {
+    id: "6",
+    email: "henrique@empresa.com",
+    name: "Henrique",
+    password: "12345456789101112",
+  },
+];

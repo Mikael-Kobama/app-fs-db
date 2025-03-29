a861ed4a9fa649d5c5f8d90a5f2f8cf82849dc80
Sr. Kobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-03-28 22:57:20 -0300

    update

--- src/app/components/template/Page.tsx
+++ src/app/components/template/Page.tsx
@@ -1,11 +1,14 @@
+import Menu from "./Menu";
+
 export interface PageProps {
   children: any;
 }
 
 export default function Page(props: PageProps) {
   return (
-    <>
-      <div>{props.children}</div>
-    </>
+    <div className="flex ">
+      <Menu />
+      <main className="flex-1 p-7">{props.children}</main>
+    </div>
   );
 }
